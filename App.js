a062f47e713044a029b642c017e13d35c7ff0584
Aryaman Rishabh <aryamanrishabh@gmail.com>
2021-08-26 23:34:23 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [day, setDay] = useState(1);
+  const [show, setShow] = useState(false);
+
+  const increase = () => {
+    setDay(day + 1);
+  };
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Hello {day}! :)</Text>
+      <Button onPress={increase} title="Increment " /> */}
+      {show ? <Text>Suhani Pagal ;P</Text> : null}
+      <Button onPress={toggleShow} title="CLick Me! " />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
